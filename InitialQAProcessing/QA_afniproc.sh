09f38b627ad3f2a4ff87854d2339021d9036919e
handwerkerd <7406227+handwerkerd@users.noreply.github.com>
2022-03-10 12:24:00 -0500

    Added aic criteria to tedana calls in QA_afniproc

--- InitialQAProcessing/QA_afniproc.sh
+++ InitialQAProcessing/QA_afniproc.sh
@@ -60,6 +60,7 @@ echo \
  "  -align_opts_aea -cost lpc+ZZ -check_flip" \\$'\n' \
  "  -volreg_align_to MIN_OUTLIER -volreg_align_e2a" \\$'\n' \
  "  -combine_method m_tedana" \\$'\n' \
+ "  -combine_opts_tedana --tedpca aic" \\$'\n' \
  "  -regress_censor_motion 0.2 -regress_censor_outliers 0.05" \\$'\n' \
  "  -regress_stim_times ./stimfiles/VisProc_Times.1D" \\$'\n' \
  "                     ./stimfiles/FalVisProc_Times.1D" \\$'\n'  \
@@ -73,16 +74,12 @@ echo \
    -gltsym \'SYM: +AudWord -FalAudWord\' -glt_label 2 Aud-FalAud \\$'\n' \
    -gltsym \'SYM: +AudWord +VisWord -FalAudWord -FalVisWord\' -glt_label 3 Word-NonWord \\$'\n' \
    -gltsym \'SYM: -AudWord +VisWord -FalAudWord +FalVisWord\' -glt_label 4 Vis-Aud \\$'\n' \
-   -gltsym \'SYM: +Keypress\' -glt_label 5 Keypress \\$'\n' \
  "  -regress_est_blur_epits -regress_est_blur_errts" \\$'\n' \
  "  -regress_apply_mot_types demean deriv" \\$'\n' \
  "  -regress_reml_exec -html_review_style pythonic" \\$'\n' \
  "  -execute" \
     >> ${subj_id}_QA_WNW_sbatch.txt
 
-# Note: This assignment of a jobid from sbatch works on biowulf, but not elsewhere.
-#   See: https://hpc.nih.gov/docs/job_dependencies.html
-WNWjobID=$(sbatch --time 6:00:00 --cpus-per-task=8 --mem=24G --error=slurm_QA_WNW.e --output=slurm_${subj_id}_QA_WNW.o ${subj_id}_QA_WNW_sbatch.txt)
 
 
 
@@ -113,10 +110,11 @@ for runid in  movie_run-1 movie_run-2 movie_run-3 breathing_run-1 breathing_run-
         "  -echo_times 13.44 31.7 49.96 -reg_echo 2" \\$'\n' \
         "  -tcat_remove_first_trs 0" \\$'\n' \
         "  -tcat_remove_last_trs 5" \\$'\n' \
-        "  -align_epi_ext_dset ../WNW/${subj_id}.results/final_epi_vr_base_min_outlier+orig" \\$'\n' \
-        "  -align_opts_aea -cost lpc+ZZ  -check_flip"  \\$'\n' \
-        "  -volreg_align_to MIN_OUTLIER -volreg_align_e2a"  \\$'\n' \
+        "  -align_opts_aea -cost lpc+ZZ"  \\$'\n' \
+        "  -volreg_base_dset ../WNW/${subj_id}.results/vr_base_min_outlier+orig" \\$'\n' \
+        "  -volreg_align_e2a"  \\$'\n' \
         "  -combine_method m_tedana" \\$'\n' \
+        "  -combine_opts_tedana --tedpca aic" \\$'\n' \
         "  -regress_censor_motion 0.2 -regress_censor_outliers 0.05" \\$'\n' \
         "  -regress_opts_3dD -jobs 8" \\$'\n' \
         "  -regress_est_blur_epits -regress_est_blur_errts" \\$'\n' \
@@ -128,7 +126,12 @@ for runid in  movie_run-1 movie_run-2 movie_run-3 breathing_run-1 breathing_run-
   fi
 done
 
+
+# Note: This assignment of a jobid from sbatch works on biowulf, but not elsewhere.
+#   See: https://hpc.nih.gov/docs/job_dependencies.html
+WNWjobID=$(sbatch --time 6:00:00 --cpus-per-task=8 --mem=24G --error=slurm_QA_WNW.e --output=slurm_${subj_id}_QA_WNW.o ${subj_id}_QA_WNW_sbatch.txt)
 moviebreath_jobID=$(swarm --time 6:00:00 --dependency=afterok:${WNWjobID} -g 24 -t 8 -m afni --merge-output --job-name moviebreath ${subj_id}_QA_moviebreath_swarm.txt)
+
 
 cd $rootdir
 if [ -f ${subj_id}_QA_jobhist_sbatch.txt ]; then
@@ -140,6 +143,7 @@ touch ${subj_id}_QA_jobhist_sbatch.txt
 echo '#!/bin/sh' >> ${subj_id}_QA_jobhist_sbatch.txt
 echo "jobhist ${WNWjobID} > ${subj_id}_jobhist_results.txt " >> ${subj_id}_QA_jobhist_sbatch.txt
 echo "jobhist ${moviebreath_jobID} >> ${subj_id}_jobhist_results.txt " >> ${subj_id}_QA_jobhist_sbatch.txt
+
 
 sbatch --dependency=afterany:${moviebreath_jobID} --time 00:30:00 --cpus-per-task=1 --partition=norm,quick ${subj_id}_QA_jobhist_sbatch.txt
 
