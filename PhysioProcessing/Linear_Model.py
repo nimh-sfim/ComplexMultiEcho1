794b0a11a03b7cbd70b268c4e5f53cced7c20ed1
Micah Holness <micahh222@gmail.com>
2022-04-18 23:16:42 -0400

    update 3

--- PhysioProcessing/Linear_Model.py
+++ PhysioProcessing/Linear_Model.py
@@ -3,13 +3,16 @@
 import numpy as np
 import pandas as pd
 import os
-from sklearn.linear_model import LinearRegression, ARDRegression
-from sklearn.preprocessing import PolynomialFeatures
+from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt
 from scipy import stats
+import subprocess
+import argparse
+import json
+import sys
 
 """
-Implements multiple linear regression
+Implements multiple linear modeling & multivariate modeling
 
 Inputs (multiple):
 Regressor pandas dataframe:
@@ -34,64 +37,129 @@ T=Time
 N=number of nuisance regressors
 """
 
-root = '/Users/holnessmn/Desktop/BIDS_conversions/'
+"""
+sub=sub-01
+task=wnw
+run=1
 
-regress = os.path.join(root, "sub-01_physios/wnw/run1/RegressorModels_wnw_r1.tsv")
-ica = os.path.join(root, "sub-01_physios/wnw/run1/ica_mixing.tsv")
+Parser call:
+python3 /Users/holnessmn/Desktop/BIDS_conversions/Linear_Model.py \
+--regressors /Users/holnessmn/Desktop/BIDS_conversions/${sub}_physios/${task}/run${run}/${sub}_RegressorModels_${task}_run-${run}.tsv \
+--ica_mixing /Users/holnessmn/Desktop/BIDS_conversions/${sub}_physios/${task}/run${run}/ica_mixing.tsv \
+--prefix /Users/holnessmn/Desktop/BIDS_conversions/${sub}_physios/${task}/run${run}/${sub}_LinearModel_${task}_run-${run}
 
-# General question: How does each X-regressor (noise model) match each Y-model (ICA component ts)?
+"""
 
-# get the components -> Y-model
+parser = argparse.ArgumentParser()
+parser.add_argument("--regressors", dest="regressors", help="Regressor Model file", type=str)
+parser.add_argument("--ica_mixing", dest="ica_mixing", help="ICA mixing matrix", type=str)
+parser.add_argument("--prefix", dest="prefix", help="File Prefix & redirected output", type=str)
+
+ARG = parser.parse_args()
+
+if ARG.regressors and os.path.isfile(ARG.regressors):
+    regress = ARG.regressors
+else:
+    raise Exception(f"This file/filepath {ARG.regressors} does not exist!!!")
+
+if ARG.ica_mixing and os.path.isfile(ARG.ica_mixing):
+    ica = ARG.ica_mixing
+else:
+    raise Exception(f"This file/filepath {ARG.ica_mixing} does not exist!!!")
+
+if ARG.prefix and type(ARG.prefix) == str:
+    prefix = ARG.prefix
+else:
+    raise Exception(f"Not a string {ARG.prefix} !!!")
+
+
+# General question: How does each X-regressor (noise model) match each Y-comp (ICA component ts)?
+
+# Y-Data #
+# Read in the ICA components
 ica_tsv = pd.read_csv(ica, sep='\t')
-ica_tsv.columns        # ['ICA_00','ICA_01',...]
-#print(ica_tsv.head)
-icamixlist = [ica_tsv[i] for i in ica_tsv.columns]     # indexed list with each component timeseries as sub-list, len = 340 (timesteps/volumes)
-#print(np.array(icamixlist))     # GOOD
+# ['ICA_00','ICA_01',...]
+ica_tsv.columns
+# multi-dimensional list with each component timeseries as sub-list, len = Num of timesteps/volumes
+icamixlist = [ica_tsv[i] for i in ica_tsv.columns]     
+
 print("Number of components: ", len(np.array(icamixlist)))
 
-# get the regressors -> X-data  (22 noise regressors + intercept ts), len = 23
+# X-Data #
+# Read in the Noise Regressors          (24 noise regressors + intercept ts), len = 25
 regres_tsv = pd.read_csv(regress, sep='\t')
-#print(regres_tsv.head)
-regres_tsv.columns        # Regressor models [m1, m2, m3, m4, m5, mN, ...]
-nphlmlist = [regres_tsv[i] for i in regres_tsv.columns[1:24]]     # indexed list with each component timeseries as sub-list, len = 340 (timesteps/volumes)
-#print(np.array(nphlmlist))      # GOOD
+# Regressor models ['cardiac_sin1','cardiac_cos1',...'WM_e','Csf_vent']
+regres_tsv.columns
+# multi-dimensional list with each regressor timeseries as sub-list, len = Num of timesteps/volumes (indexed by TRs...)
+nphlmlist = [regres_tsv[i] for i in regres_tsv.columns[1:26]]     
+
 print("Number of noise regressors: ", len(np.array(nphlmlist)))
 
-coefficient_matrix = []
-R_sq = []
-p_value = []
 
-# Check the model fit visually
 def visual_check(icamixlist, nphlmlist):
-    # get scores for each ICA component
+    """
+    Visual Check for each regressor model fit over each component
+
+    Input: ICA mixing matrix, Regressor Models
+    Output: Visual Plot of each Regressor Model (red) over each ICA component (blue)
+
+    """
+
+    # Iterate through each ICA component & Get Scores of Fit
     for idx, i in enumerate(icamixlist):
 
-        # X = regressors models, Y = ica component, x.shape = (24,340), y.shape = (340)
-        x, y = np.array(nphlmlist)[1:24], np.array(i)     # includes intercept of 1's
-        #print(x)
-        #print(y)
+        # X = regressors models, Y = ica component, x.shape = (25,nvols), y.shape = (nvols)
+        x, y = np.array(nphlmlist)[1:26], np.array(i)     # includes intercept of 1's
 
+        # Iterate through each regressor
         for xi in x:
-            #print(xi)
-            #print(xi.reshape(-1, 1))
+
+            # find significant p-value of each regressor (Xi) fit to component (y)
+            sign = stats.linregress(xi, y).pvalue
             xi = xi.reshape(-1, 1)
             linear_model = LinearRegression().fit(xi, y)       # fit x-regressor data to y ica data
 
-            # print summary of data
+            # print summary of data:
             # intercept will be 0, bcuz sine wave (x,y modulate around 0)
-            print("Coefficient of determination (R-sq): ", linear_model.score(xi, y), "Intercept: ", linear_model.intercept_, "Coefficients (scalar magnitudes): ", linear_model.coef_, "p-value: ", stats.linregress(xi, y).pvalue)
+            print("Coefficient of determination (R-sq): ", linear_model.score(xi, y), "Intercept: ", linear_model.intercept_, "Coefficients (scalar magnitudes): ", linear_model.coef_, "p-value: ", sign)     
 
-            plt.plot(y, 'b-')      # actual data: ICA component
-            plt.plot(linear_model.predict(xi), 'r-')        # predicted ICA comp (output) based on the X-regressors (predictors)
+            # plot the data -> ICA component (blue) = Y,  predicted ICA component based on (fitted to) X-regressor data
+            plt.plot(y, 'b-')
+            plt.plot(linear_model.predict(xi), 'r-')
+            plt.legend(['ICA Component', 'Noise Regressor'], loc='best')
             plt.show()
 
             # Note: 
             # coefficient of determination -> will tell you how likely an ICA component is noise
             # coefficients -> will tell you which noise regressor model the ICA component matches
+
 #visual_check(icamixlist, nphlmlist)
 
 
 def linear_model(icamixlist, nphlmlist):
+    """
+    Compute Linear Model
+
+    Equation: Y = XB + E
+    - Y = each ICA component
+    - X = Design (Regressor) matrix
+    - B = Weighting Factors (solving for B)
+    - E = errors (Y - Y_pred OR Y - XB)
+
+    Y1 = B0 + B1X4 + B2X5 + B3X6
+    Yn = B0 + B1X1 + B2X2 + B3X3
+
+    Y1 = (1 + X2 + X4 + X6) * (B0) + E1
+    Yn = (1 + X1 + X3 + X5) * (B1) + E2
+    (nx1)       (nx4)         (2x1) (nx1)
+
+    Input: ICA mixing matrix, Regressor Models
+    Output: Coefficient of determination (R-squared), B-Coefficients (list of scalar magnitudes)
+
+    """
+    # initialize
+    R_sq, coefficient_matrix = [], []
+
     # get scores for each ICA component
     for idx, i in enumerate(icamixlist):
 
@@ -116,36 +184,108 @@ def linear_model(icamixlist, nphlmlist):
 
     return coefficient_matrix, R_sq
 
-#linear_model(icamixlist, nphlmlist)
 
 def sign(icamixlist, nphlmlist):
+    """
+    Get significance of each regressor fit to ICA component
+
+    Input: 
+    Output: P-value DataFrame (p-values, cols = regressors, rows = components)
+    
+    """
     sig_arr = []
     for idx, i in enumerate(icamixlist):
         x, y = np.array(nphlmlist), np.array(i)
         for xi in x:    # regressors = cols
             p_value = stats.linregress(xi, y).pvalue
             sig_arr.append(p_value)
-    sig_array = np.array(sig_arr).reshape(30,23)      #30 comps = rows, 23 regressors = columns
+    sig_array = np.array(sig_arr).reshape(len(np.array(icamixlist)),len(np.array(nphlmlist)))      #num comps = rows, num regressors = columns
+    
     return sig_array
-#sign(icamixlist, nphlmlist)
+
 
 def convert():
-    coefficient_matrix, R_sq = linear_model(icamixlist, nphlmlist)
-    pval = sign(icamixlist, nphlmlist)
-    # convert to np array, then to pandas dataframe
-    r_squared = ["R-squared"]
-    regressor_columns = ["intercept","cardiac_sin1","cardiac_cos1","cardiac_sin2","cardiac_cos2","resp_sin1","resp_cos1","resp_sin2","resp_cos2","ecg_hrv","resp_rvt","roll_dmn","pitch_dmn","yaw_dmn","dS_dmn","dL_dmn","dP_dmn","roll_drv","pitch_drv","yaw_drv","dS_drv","dL_drv","dP_drv"]
-    # R-sq [0] & Regressor coefficients [1:22]
-    RSQ_df = pd.DataFrame(np.array(R_sq), columns=r_squared) 
-    Cmag_df = pd.DataFrame(np.array(coefficient_matrix), columns=regressor_columns) 
-    Score_df = RSQ_df.join(Cmag_df)
-    Sig_df = pd.DataFrame(pval, columns=regressor_columns)
-    
-    print(Score_df)
-    print(Sig_df)
+    """
+    Convert Statistics to .Tsv.gz/.json files
 
-    # export DataFrames to tsv file
-    #Score_df.to_csv('ScoresICs_LM.tsv', sep="\t")
-    #Sig_df.to_csv('SigICs_LM.tsv', sep="\t")
+    Input: ICA mixing matrix, Regressor Models
+    Output: 
+        1) ScoresICs.tsv -> R-squared & Coefficient Magnitudes for each IC fit to each Regressor Model (Columns)
+        2) SigICs.tsv -> P-values (significance) for each IC fit to each Regressor Model (Rows)
+        3) SigICs.json -> Dictionary of Significant (p < 0.001) ICs (Values) and the Regressor Model they fit to (Keys)
+        4) SigICs.txt -> List of all significant ICs
+
+    """
+
+    # Get the DataFrames as variables
+    coefficient_matrix, R_sq = linear_model(icamixlist, nphlmlist)
+    pvals = sign(icamixlist, nphlmlist)
+
+    print("Coeff mat: ",coefficient_matrix, "R-sq: ",R_sq, "P-val: ",pvals)
+
+    # 1. generates stacked columns of statistics arrays (as columns)
+    scores = np.column_stack((np.array(R_sq),np.array(coefficient_matrix)))
+
+    columns = ["intercept",
+                "roll_dmn","pitch_dmn","yaw_dmn","dS_dmn","dL_dmn","dP_dmn",
+                "roll_drv","pitch_drv","yaw_drv","dS_drv","dL_drv","dP_drv",
+                "cardiac_sin1","cardiac_cos1","cardiac_sin2","cardiac_cos2",
+                "resp_sin1","resp_cos1","resp_sin2","resp_cos2",
+                "ecg_hrv","resp_rvt",
+                "WM_e","Csf_vent"
+                ]
+
+    # 2. convert/join all regressors to 1 dataframe
+    df_scores = pd.DataFrame(scores, 
+                    columns=["R-squared"]+columns)
+    print(df_scores)
+
+    df_sigs = pd.DataFrame(np.array(pvals), columns=columns)
+    print(df_sigs)
+
+    # export Statistics DataFrames to tsv files
+    df_scores.to_csv(f"{prefix}_ScoresICs.tsv", sep="\t", index=False)
+    df_sigs.to_csv(f"{prefix}_SigICs.tsv", sep="\t", index=False)
+
+    return df_sigs
+
 convert()
 
+# Get significant ICs
+def significant_ICs():
+
+    df_sigs = convert()
+
+    # Extract Significant Components (p < 0.001)
+    Sig_dict = {}
+    for sig in [sig for sig in df_sigs.columns]:
+        Sig_dict[sig] = [idx for idx, i in enumerate(df_sigs[sig]) if i < 0.001]
+    Sig_dict=Sig_dict
+
+    # DUMP Sig ICs (for each regressor) to JSON file -> extract with "jq" command
+    with open(f"{prefix}_SigICs.json", "w") as jf:
+        json.dump(Sig_dict, jf, indent=2)
+
+    # Get list of sig ICs that fit the entire Regressor model
+    List = list(Sig_dict.values())
+    print(List)
+
+    # unpack list of sig ICs for each regressor & get unique (1 frequency) array
+    motion_dmn_fit = [*List[1],*List[2],*List[3],*List[4],*List[5],*List[6]]
+    motion_drv_fit = [*List[7],*List[8],*List[9],*List[10],*List[11],*List[12]]
+    cardiac_retro = [*List[13],*List[14],*List[15],*List[16]]
+    cardiac_hrv = [*List[21]]
+    resp_retro = [*List[17],*List[18],*List[19],*List[20]]
+    resp_rvt = [*List[22]]
+    wm_fit = [*List[23]]
+    csf_fit = [*List[24]]
+
+    # convert noise regressors to formatted list & export to .txt file (for use in GLMs)
+    for i in (motion_dmn_fit,motion_drv_fit,cardiac_retro,cardiac_hrv,resp_retro,resp_rvt,wm_fit,csf_fit):
+        i = np.unique(np.array(i)).tolist()
+        subprocess.check_output(f"echo {i} >> {prefix}_SigICs.txt", shell=True)
+
+significant_ICs()
+
+# Note: regressor model forms with smoother lines (e.g., HRV/RVT) won't have as high of a coefficient mag or R-sq as a regressor with more noise...
+# denoising with Nordic (with blurring) might lead to cleaner components with LESS noise/power (leading to higher fits with RVT/HRV regressor forms)
---
+++ PhysioProcessing/Physio_Proc.sh
@@ -0,0 +1,262 @@
+# Python
+
+# Physio Proc
+
+# Subj that don't have all required files or something wrong with acquisition: sub-03 & sub-09, sub-04 (CUT!)
+echo "Enter subject: (Ex: sub-01)" 
+read sub
+echo "${sub} physiological files are being processed"
+
+scripts="/Users/holnessmn/Desktop/BIDS_conversions/"
+root="/Users/holnessmn/Desktop/BIDS_conversions/${sub}_physios/"
+ 
+if [ -d ${root}Originals ]; then 
+    echo "Originals have been converted. Ready to run."
+else 
+    echo "No Originals directory. Convert with 'conversions.sh' script"
+fi
+
+echo "Enter process to call: (Ex: trim)"
+read call
+
+####################
+# Calls to be made #
+# Shell Variables #
+####################
+trim_file() {python3 ${scripts}file_trimmer.py \
+    --filepath ${root}Originals/${sub}_task-${task}_run-${run}_physio.tsv.gz \
+    --outpath ${root}}
+
+down_smple() {python3 ${scripts}downsampler.py \
+    --filepath ${root}${sub}_task-${task}_run-${run}_physio.tsv.gz}
+
+over_lay() {python3 ${scripts}overlay.py \
+    --filepath ${root}${sub}_task-${task}_run-${run}_resp_down.tsv.gz \
+    --run_ideal ${ideal}}
+
+Niphlm_regressors() {python3 ${scripts}NiPhlem_regressors.py \
+    --tsv ${root}${sub}_task-${task}_run-${run}_physio.tsv.gz \
+    --json ${root}Originals/${sub}_task-${task}_run-${run}_physio.json \
+    --n_vols $nvols \
+    --motion_demean $dmn \
+    --motion_deriv $drv \
+    --WM $wm \
+    --CSF $csf \
+    --prefix ${root}${task}/run${run}/${sub}_RegressorModels_${task}_run-${run}}
+
+Linear_Model() {python3 ${scripts}Linear_Model.py \
+    --regressors ${root}${task}/run${run}/${sub}_RegressorModels_${task}_run-${run}.tsv \
+    --ica_mixing ${root}${task}/run${run}/ica_mixing.tsv \
+    --prefix ${root}${task}/run${run}/${sub}_LinearModel_${task}_run-${run}}
+
+####################
+
+####################
+#    Parameters    #
+####################
+# movie run 1st (A or B)
+group_A=(1 2 6 8)
+group_B=(3 4 5 7 9)
+# 3rd movie run
+group_C=(5)
+# 3rd movie run
+runs_3=(5)
+# 1 or 2 breathing runs
+runs_2=(2 3 4 5 6 7)
+runs_1=(1 8 9)
+tasks=('wnw' 'breathing' 'movie')
+####################
+
+check() {
+
+    # Check if Task directory exists
+    for task in $tasks; do
+        # Make directories - check 1st
+        if ! [ -d $root/$task ]; then mkdir $task; else echo "Task '$task' Directory exists"; fi
+    done
+}
+
+
+
+trim() {
+    # Trim
+    for task in $tasks; do
+
+        # WNW
+        if [[ $task == 'wnw' ]]; then 
+            # set variables & commands
+            runs=(1 2 3); echo "Runs $runs"
+            # make sub-directories
+            cd ${root}/${task}; if ! [ -d $root/$task/run1 ]; then mkdir run1 run2 run3; else echo "Wnw Run Directories Exist"; fi
+            cd $root
+            for run in $runs; do
+                if ! [ -f ${root}${sub}_task-${task}_run-${run}_physio.tsv.gz ]; then
+                    trim_file
+                fi
+            done
+        fi
+
+        # Movie / Breathing
+        if [[ $task == 'breathing' || $task == 'movie' ]]; then
+            # make sub-directories
+            cd ${root}/${task}; 
+            if ! [ -d $root/$task/run1 ]; then mkdir run1; runs=(1); fi
+            if [ -f ${root}Originals/${sub}_task-${task}_run-2_physio.tsv.gz ]; then mkdir run2; runs=(1 2); else runs=(1); fi
+            if [ -f ${root}Originals/${sub}_task-${task}_run-3_physio.tsv.gz ]; then mkdir run3; runs=(1 2 3); else runs=(1 2); fi
+            cd $root
+            for run in $runs; do 
+                if ! [ -f ${root}${sub}_task-${task}_run-${run}_physio.tsv.gz ]; then
+                    trim_file
+                fi
+            done
+        fi
+    done
+}
+
+
+downsample() {
+    tasks=('movie' 'breathing')
+    runs=(1 2)
+    # Downsample
+    for task in $tasks; do
+        for run in $runs; do
+            if ! [ -f ${root}${sub}_task-${task}_run-${run}_resp_down.tsv.gz ]; then
+                down_smple
+            fi
+        done
+    done
+}
+
+
+overlay() {
+    tasks=('movie' 'breathing')
+    runs=(1 2)
+    # Overlay
+    for task in $tasks; do
+        for run in $runs; do
+            # find ideal run pattern
+            if [[ $task == 'movie' ]]; then echo "task is movie";
+                if [[ $run == 1 ]]; then echo "run is 1"
+                    # $array[(Ie)$var]  -> index array & find exact match to $var
+                    if (($group_A[(Ie)$sub[-1]])); then
+                        ideal=A
+                    elif (($group_B[(Ie)$sub[-1]])); then
+                        ideal=B
+                    fi
+                elif [[ $run == 2 ]]; then echo "run is 2"
+                    if (($group_A[(Ie)$sub[-1]])); then
+                        ideal=B
+                    elif (($group_B[(Ie)$sub[-1]])); then
+                        ideal=A
+                    fi
+                elif [[ $run == 3 ]]; then echo "run is 3"
+                    if (($group_A[(Ie)$sub[-1]])); then
+                        ideal=C
+                    fi
+                fi
+            elif [[ $task == 'breathing' ]]; then echo "task is breathing";
+                ideal=A
+            fi
+            echo "Ideal Run pattern is: $ideal";
+            over_lay
+        done
+    done
+}
+
+
+# GLOBUS: transfer 4 files for Linear Regression: ica_mixing.tsv, motion_demean.1D, motion_deriv.1D, mean.ROI.FSWe.1D, mean.ROI.FSvent.1D
+
+
+# ~ NiPhlm Regressors ~ #
+calc_regressors() {
+    for task in $tasks; do
+        echo "Task is ${task}. Subj is ${sub}. Enter num of vols: "
+        # enter num of vols without last 5 noise files
+        read nvols
+        if [[ $task == 'wnw' ]]; then runs=(1 2 3)
+        # default: 3 runs (wnw)
+            for run in $runs; do
+                # differential .1D noise directory paths (by task)
+                dmn=${root}${task}/motion_demean.1D
+                drv=${root}${task}/motion_deriv.1D
+                wm=${root}${task}/mean.ROI.FSWe.1D
+                csf=${root}${task}/mean.ROI.FSvent.1D
+                if ! [ -f ${root}${task}/run${run}/${sub}_RegressorModels_${task}_run-${run}.tsv ]; then
+                    # process & model regressors & compute Linear Regression
+                    Niphlm_regressors
+                else
+                    echo "Regressor Model file exists"
+                fi
+            done
+        elif [[ $task == 'movie' ]]; then
+        # default: 2 runs (movie)
+            if (($runs_3[(Ie)$sub[-1]])); then runs=(1 2 3); else runs=(1 2); fi
+            for run in $runs; do
+                # differential .1D noise directory paths (by task)
+                dmn=${root}${task}/run$run/motion_demean.1D
+                drv=${root}${task}/run$run/motion_deriv.1D
+                wm=${root}${task}/run$run/mean.ROI.FSWe.1D
+                csf=${root}${task}/run$run/mean.ROI.FSvent.1D
+                if ! [ -f ${root}${task}/run${run}/${sub}_RegressorModels_${task}_run-${run}.tsv ]; then
+                    # process & model regressors & compute Linear Regression
+                    Niphlm_regressors
+                else
+                    echo "Regressor Model file exists"
+                fi
+            done
+        elif [[ $task == 'breathing' ]]; then
+        # default: 1 run (breathing)
+            if (($runs_2[(Ie)$sub[-1]])); then runs=(1 2); else runs=(1); fi
+            for run in $runs; do
+                # differential .1D noise directory paths (by task)
+                dmn=${root}${task}/run$run/motion_demean.1D
+                drv=${root}${task}/run$run/motion_deriv.1D
+                wm=${root}${task}/run$run/mean.ROI.FSWe.1D
+                csf=${root}${task}/run$run/mean.ROI.FSvent.1D
+                if ! [ -f ${root}${task}/run${run}/${sub}_RegressorModels_${task}_run-${run}.tsv ]; then
+                    # process & model regressors & compute Linear Regression
+                    Niphlm_regressors
+                else
+                    echo "Regressor Model file exists"
+                fi
+            done
+        fi
+    done 
+}
+
+
+# ~ Linear Model ~ #
+linearmodel() {
+    for task in $tasks; do
+        if [[ $task == 'wnw' ]]; then runs=(1 2 3)
+        elif [[ $task == 'movie' ]]; then
+        # default: 2 runs (movie)
+            if (($runs_3[(Ie)$sub[-1]])); then runs=(1 2 3); else runs=(1 2); fi
+        elif [[ $task == 'breathing' ]]; then
+        # default: 1 run (breathing)
+            if (($runs_2[(Ie)$sub[-1]])); then runs=(1 2); else runs=(1); fi
+        fi
+        for run in $runs; do
+            Linear_Model
+        done
+    done
+}
+
+
+######
+#calls
+######
+$call
+
+
+###################
+# Required Order: #
+###################
+#check
+#trim
+
+#downsample
+#overlay
+
+#calc_regressors
+#linearmodel
