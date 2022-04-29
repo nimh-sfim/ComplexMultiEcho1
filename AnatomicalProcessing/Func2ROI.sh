ae4820da669b4eb0133e246dea0919dbeccbe4b9
handwerkerd <7406227+handwerkerd@users.noreply.github.com>
2022-04-29 16:16:47 -0400

    Func2ROI.sh improved documentation

--- AnatomicalProcessing/Func2ROI.sh
+++ AnatomicalProcessing/Func2ROI.sh
@@ -1,6 +1,4 @@
-# Extract ROIs & Intersect with Functional
-
-# needs --20g mem
+# Extract ROIs & Intersect with Functional contrasts
 
 subj_id=$1
 
@@ -11,16 +9,13 @@ cd StudyROIs
 
 3dcopy -overwrite ../aparc.a2009s+aseg_REN_gmrois.nii.gz tmp_all_gmROIs.nii.gz
 
-
 # 60 is ctx_lh_G_front_inf-Opercular &  62 is ctx_lh_G_front_inf-Triangul
 # combining into 1 ROI with the index 60 that we'll call ctx_lh_G_front_inf-OperTri
 3dcalc -a tmp_all_gmROIs.nii.gz -prefix tmp_all_gmROIs.nii.gz -overwrite -expr 'equals(a,62)*60 + not(equals(a,62))*a'
 
-
 # 135 is ctx_rh_G_front_inf-Opercular &  137 is ctx_rh_G_front_inf-Triangul
 # combining into 1 ROI with the index 60 that we'll call ctx_rh_G_front_inf-OperTri
 3dcalc -a tmp_all_gmROIs.nii.gz -prefix tmp_all_gmROIs.nii.gz -overwrite -expr 'equals(a,137)*135 + not(equals(a,137))*a'
-
 
 # get ROI indices
 # ROIs is for Word-Nonword contrasts
@@ -42,33 +37,16 @@ ROIidxVS=(122 197 92 167)
 ROIidxVSlabels=(ctx_lh_S_temporal_transverse ctx_rh_S_temporal_transverse ctx_lh_S_calcarine ctx_rh_S_calcarine)
 
 
-####
-
+# Put all ROIs and labels in one list
 ROIidxlist=(${ROIidxVS[@]} ${ROIidxWNW[@]})
 ROIidxlabels=(${ROIidxVSlabels[@]} ${ROIidxWNWlabels[@]})
-# ROIidxlist=(122 197 92 167 \
-#              69 144 121 196 \
-#              86 161 60 135 \
-#              75 150 6 26 \
-#              59 134 78 153 \
-#              14 31 85 160 \
-#              120 195 67 142)
-
-# get ROI labels
-# ROIidxlabels=(ctx_lh_S_temporal_transverse ctx_rh_S_temporal_transverse ctx_lh_S_calcarine ctx_rh_S_calcarine \
-#               ctx_lh_G_oc-temp_lat-fusifor ctx_rh_G_oc-temp_lat-fusifor ctx_lh_S_temporal_sup ctx_rh_S_temporal_sup \
-#               ctx_lh_G_temporal_middle ctx_rh_G_temporal_middle ctx_lh_G_front_inf-OperTri ctx_rh_G_front_inf-OperTri \
-#               ctx_lh_G_parietal_sup ctx_rh_G_parietal_sup Left-Cerebellum-Cortex Right-Cerebellum-Cortex \
-#               ctx_lh_G_cuneus ctx_rh_G_cuneus ctx_lh_G_precuneus ctx_rh_G_precuneus \
-#               Left-Hippocampus Right-Hippocampus ctx_lh_G_temporal_inf ctx_rh_G_temporal_inf \
-#               ctx_lh_S_temporal_inf ctx_rh_S_temporal_inf ctx_lh_G_occipital_middle ctx_rh_G_occipital_middle)
 
 echo Length ROIidxlist is ${#ROIidxlist[@]}
 echo Length ROIidxlabels is ${#ROIidxlabels[@]}
 
 numROIs=${#ROIidxlist[@]}
 
-# Make the desired ROI a stand-alone file:
+# Make each desired ROI a stand-alone file:
 for ((idx=0; $idx<${numROIs}; idx++)); do
     echo idx $idx ROIidx  ${ROIidxlist[$idx]} label ${ROIidxlabels[$idx]}
     3dcalc -overwrite -a tmp_all_gmROIs.nii.gz  \
@@ -88,11 +66,10 @@ done
 
 # Identify voxels that are in more than one ROI and remove them from all ROIs
 3dTstat -overwrite -nzcount -prefix tmp_ROIs_EPIres_nzcount.nii.gz tmp_ROIs_EPIres.nii.gz
-
 3dcalc -overwrite -a tmp_ROIs_EPIres_nzcount.nii.gz \
 -prefix ROI_1vox_per_mask.nii.gz -expr '1*equals(a,1)'
 
-
+# Make separate files with EPI resolution masks for each ROI
 for ((idx=0; $idx<${numROIs}; idx++)); do
     echo idx $idx ROIidx  ${ROIidxlist[$idx]} label ${ROIidxlabels[$idx]}
     3dcalc -a tmpROI_EPIres_${ROIidxlabels[$idx]}.nii.gz \
@@ -102,30 +79,18 @@ for ((idx=0; $idx<${numROIs}; idx++)); do
        -overwrite
 done
 
+# Cominbe the EPI resolution anatomical masks into one volume
 if [ -f tmpROI_nooverlap_EPIres_all.nii.gz ]; then
   rm tmpROI_nooverlap_EPIres_all.nii.gz
 fi
 3dTcat -prefix tmpROI_nooverlap_EPIres_all.nii.gz tmpROI_nooverlap_EPIres_*.nii.gz
 3dTstat -overwrite -datum short -sum -prefix ROIs_EPIres.nii.gz tmpROI_nooverlap_EPIres_all.nii.gz 
 
-
-# Make functional masks for Visual-Audio
-
-
-
-
 # Create Functional ROIs:
-# PRECAUTIONS: no 2 ROIs overlapping (same voxels), 
-# intersect the functional contrast (Stats.REML) w/ anatomical ROIs
-# inset = 3D volume of values (functionally-derived values like Stats.REML)
-# threshold for dataset values (use p < 0.001?)
-# volthr = minimum number of voxels for an ROI (want at least 5 voxels)
-# refset -> has labletable?
-# output: map of ROIs
 
-# NOTE CURRENTLY RUNNING ON THE DENOISED STATS FILE. WILL WANT TO USE OPTIMALLY COMBINED OR 2nd ECHO
-# NOTE THRESHOLD IS ARBITRARY t>3
 
+# Realized with hindsight that 3dROIMaker didn't do everything I thought it would
+# Might be what I need to match numbers to labels, but still not sure
 # cp ../aparc.a2009s+aseg_REN_all.niml.lt ./ROIs_EPIres.niml.lt
 
 # word-nonword T-stat: 23 visual-audio T-stat: 26
@@ -136,7 +101,7 @@ fi
 #   -refset ROIs_EPIres.nii.gz -thresh 3.3 -volthr 5 -prefix VisAud_FuncROIs
 
 
-# Separately making a functional mask with clusters that have at least 5 voxels.
+# Making a functional mask with clusters that have at least 5 voxels.
 # Note: Using an uncorrected threshold of p<0.01 because the goal is to get functionally localized
 #  voxels in each ROI. It's better to have things that are slightly below threshold than to exclude
 #  voxels or ROIs that might have more robust responses with other denoising pipelines
@@ -178,7 +143,7 @@ for ((idx=0; $idx<${#ROIidxVS[@]}; idx++)); do
    -prefix tmpfuncROI_${ROIidxVSlabels[$idx]}.nii.gz
 done
 
-# Combine all the function ROIS into one mask
+# Combine all the functional ROIS into one mask
 if [ -f tmpfuncROI_EPIres_all.nii.gz ]; then
   rm tmpfuncROI_EPIres_all.nii.gz
 fi
