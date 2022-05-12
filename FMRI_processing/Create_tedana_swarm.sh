e27c9d51f9384a9b090b6640ef14ffb7147c12aa
handwerkerd <7406227+handwerkerd@users.noreply.github.com>
2022-05-12 16:38:37 -0400

    playing with tedana variance

--- FMRI_processing/Create_tedana_swarm.sh
+++ FMRI_processing/Create_tedana_swarm.sh
@@ -9,7 +9,7 @@ touch WNW_tedana_sbatch.txt
 
 rootdir=`pwd`
 
-sublist=(01 02 03 04 05 06 07 08 09 10 11)
+sublist=(01 02 03 04 05 06 07 08 09 10 11 12 13)
 
 runlist=(01 02 03)
 echo_times="13.44 31.7 49.96"
@@ -23,11 +23,67 @@ cat << EOF >> WNW_tedana_sbatch.txt
   tedana -d pb0?.$subj.r$run.e*.volreg+orig.HEAD \
   -e $echo_times \
   --mask full_mask.$subj+orig.HEAD \
-  --tedpca 0.88 \
-  --out-dir tedana_v88_r$run --convention orig
+  --tedpca 75 \
+  --out-dir tedana_c75_r$run --convention orig
 EOF
  done
 done
+for subidx in ${sublist[@]}; do
+  subj=sub-${subidx}
+  for run in ${runlist[@]}; do
+cat << EOF >> WNW_tedana_sbatch.txt
+ source ~/InitConda.sh; \
+  cd ${rootdir}/${subj}/afniproc_orig/WNW/${subj}.results/; \
+  tedana -d pb0?.$subj.r$run.e*.volreg+orig.HEAD \
+  -e $echo_times \
+  --mask full_mask.$subj+orig.HEAD \
+  --tedpca 70 \
+  --out-dir tedana_c70_r$run --convention orig
+EOF
+ done
+done
+for subidx in ${sublist[@]}; do
+  subj=sub-${subidx}
+  for run in ${runlist[@]}; do
+cat << EOF >> WNW_tedana_sbatch.txt
+ source ~/InitConda.sh; \
+  cd ${rootdir}/${subj}/afniproc_orig/WNW/${subj}.results/; \
+  tedana -d pb0?.$subj.r$run.e*.volreg+orig.HEAD \
+  -e $echo_times \
+  --mask full_mask.$subj+orig.HEAD \
+  --tedpca 65 \
+  --out-dir tedana_c65_r$run --convention orig
+EOF
+ done
+done
+# for subidx in ${sublist[@]}; do
+#   subj=sub-${subidx}
+#   for run in ${runlist[@]}; do
+# cat << EOF >> WNW_tedana_sbatch.txt
+#  source ~/InitConda.sh; \
+#   cd ${rootdir}/${subj}/afniproc_orig/WNW/${subj}.results/; \
+#   tedana -d pb0?.$subj.r$run.e*.volreg+orig.HEAD \
+#   -e $echo_times \
+#   --mask full_mask.$subj+orig.HEAD \
+#   --tedpca 0.71 \
+#   --out-dir tedana_v71_r$run --convention orig
+# EOF
+#  done
+# done
+# for subidx in ${sublist[@]}; do
+#   subj=sub-${subidx}
+#   for run in ${runlist[@]}; do
+# cat << EOF >> WNW_tedana_sbatch.txt
+#  source ~/InitConda.sh; \
+#   cd ${rootdir}/${subj}/afniproc_orig/WNW/${subj}.results/; \
+#   tedana -d pb0?.$subj.r$run.e*.volreg+orig.HEAD \
+#   -e $echo_times \
+#   --mask full_mask.$subj+orig.HEAD \
+#   --tedpca 0.88 \
+#   --out-dir tedana_v88_r$run --convention orig
+# EOF
+#  done
+# done
 for subidx in ${sublist[@]}; do
   subj=sub-${subidx}
   for run in ${runlist[@]}; do
@@ -42,20 +98,20 @@ cat << EOF >> WNW_tedana_sbatch.txt
 EOF
  done
 done
-for subidx in ${sublist[@]}; do
-  subj=sub-${subidx}
-  for run in ${runlist[@]}; do
-cat << EOF >> WNW_tedana_sbatch.txt
- source ~/InitConda.sh; \
-  cd ${rootdir}/${subj}/afniproc_orig/WNW/${subj}.results/; \
-  tedana -d pb0?.$subj.r$run.e*.volreg+orig.HEAD \
-  -e $echo_times \
-  --mask full_mask.$subj+orig.HEAD \
-  --tedpca 0.9 \
-  --out-dir tedana_v90_r$run --convention orig
-EOF
- done
-done
+# for subidx in ${sublist[@]}; do
+#   subj=sub-${subidx}
+#   for run in ${runlist[@]}; do
+# cat << EOF >> WNW_tedana_sbatch.txt
+#  source ~/InitConda.sh; \
+#   cd ${rootdir}/${subj}/afniproc_orig/WNW/${subj}.results/; \
+#   tedana -d pb0?.$subj.r$run.e*.volreg+orig.HEAD \
+#   -e $echo_times \
+#   --mask full_mask.$subj+orig.HEAD \
+#   --tedpca 0.9 \
+#   --out-dir tedana_v90_r$run --convention orig
+# EOF
+#  done
+# done
 for subidx in ${sublist[@]}; do
   subj=sub-${subidx}
   for run in ${runlist[@]}; do
@@ -100,4 +156,4 @@ EOF
 done
 
 
-# swarm --time 6:00:00 -g 12 -t 8 -b 6 -m afni --merge-output --job-name tedana_pca WNW_tedana_sbatch.txt
+# swarm --time 6:00:00 -g 12 -t 8 -b 3 -m afni --merge-output --job-name tedana_pca WNW_tedana_sbatch.txt
