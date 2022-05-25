6b94ead7b9ae5688e6096ff3fe2cbdf3c3889b4a
handwerkerd <7406227+handwerkerd@users.noreply.github.com>
2022-05-24 21:25:52 -0400

    Added Group_SunSignifMaps.sh

---
+++ FMRI_processing/Group_SumSignifMaps.sh
@@ -0,0 +1,101 @@
+#!/bin/bash
+
+# Calculate the significance maps per subject then add them together
+
+
+rootdir=/data/NIMH_SFIM/handwerkerd/ComplexMultiEcho1/Data
+GroupDir="${rootdir}/GroupResults/GroupMaps"
+
+
+sbjlist=(01 02 03 04 05 06 07 08 09 10 11 12 13)
+
+GLMlist=(e2_mot_CSF OC_mot_CSF orthtedana_mot_csf combined_regressors)
+
+# for each subject and run make binarized maps of voxels above threshold
+# Warp those binarized maps to a common template space
+for snum in ${sbjlist[@]}; do
+  sbj=sub-${snum}
+  warp_path="${rootdir}/${sbj}/Proc_Anat/awpy"
+  for GLM in ${GLMlist[@]}; do
+
+  cd ${rootdir}/${sbj}/GLMs/${GLM}
+  word_nword_thresh=`fdrval -qinput stats.${sbj}.${GLM}_REML+orig 31 0.05`
+  vis_aud_thresh=`fdrval -qinput stats.${sbj}.${GLM}_REML+orig 35 0.05`
+
+  3dcalc -prefix ${sbj}.${GLM}_WNW_signif.nii.gz \
+      -a stats.${sbj}.${GLM}_REML+orig'[31]' \
+      -expr "ispositive(abs(a)-${word_nword_thresh})*(ispositive(a)-isnegative(a))" \
+      -overwrite
+
+  3dcalc -prefix ${sbj}.${GLM}_VisAud_signif.nii.gz \
+      -a stats.${sbj}.${GLM}_REML+orig'[35]' \
+      -expr "ispositive(abs(a)-${vis_aud_thresh})*(ispositive(a)-isnegative(a))" \
+      -overwrite
+
+
+   3dNwarpApply -overwrite -nwarp "${warp_path}/anat.un.aff.qw_WARP.nii ${warp_path}/anat.un.aff.Xat.1D" \
+         -master ${GroupDir}/alignment_EPIgrid_template_sub-01.nii.gz \
+         -source ${sbj}.${GLM}_VisAud_signif.nii.gz \
+         -ainterp NN -short \
+         -prefix ${GroupDir}/sbj_maps/${sbj}.${GLM}_VisAud_signif_tlrc.nii.gz
+
+   3dNwarpApply -overwrite -nwarp "${warp_path}/anat.un.aff.qw_WARP.nii ${warp_path}/anat.un.aff.Xat.1D" \
+         -master ${GroupDir}/alignment_EPIgrid_template_sub-01.nii.gz \
+         -source ${sbj}.${GLM}_WNW_signif.nii.gz \
+         -ainterp NN -short \
+         -prefix ${GroupDir}/sbj_maps/${sbj}.${GLM}_WNW_signif_tlrc.nii.gz
+
+  done
+done
+
+
+cd $GroupDir
+
+# Make summary count maps
+for GLM in ${GLMlist[@]}; do
+   3dMean -prefix GroupSignifCount_WNW_${GLM}.nii.gz -sum -overwrite \
+        ./sbj_maps/sub-??.${GLM}_WNW_signif_tlrc.nii.gz
+   3dMean -prefix GroupSignifCount_VisAud_${GLM}.nii.gz -sum -overwrite \
+        ./sbj_maps/sub-??.${GLM}_VisAud_signif_tlrc.nii.gz
+done
+
+
+
+# Trying on the spatially smoothed stat maps
+cd $GroupDir/sbj_maps
+for snum in ${sbjlist[@]}; do
+  sbj=sub-${snum}
+  for GLM in ${GLMlist[@]}; do
+    3dFDR -input sm.stats.${sbj}.${GLM}_REML_tlrc.nii.gz'[30,31]' -mask ../GroupMask.nii.gz \
+      -prefix sm.${sbj}.${GLM}.WNW_qvals.nii.gz -overwrite -qval
+    # For voxels in the mask, +1 for positive coefficience with q<0.05 and -1 for negative coefficients with q<0.05
+    3dcalc \
+      -a sm.${sbj}.${GLM}.WNW_qvals.nii.gz'[0]' -b sm.${sbj}.${GLM}.WNW_qvals.nii.gz'[1]' \
+      -c ../GroupMask.nii.gz \
+      -expr "ispositive(c)*isnegative(b-0.05)*(ispositive(a)-isnegative(a))" \
+      -overwrite \
+      -prefix sm.${sbj}.${GLM}.WNW_signif.nii.gz
+
+    3dFDR -input sm.stats.${sbj}.${GLM}_REML_tlrc.nii.gz'[34,35]' -mask ../GroupMask.nii.gz \
+      -prefix sm.${sbj}.${GLM}.VisAud_qvals.nii.gz -overwrite -qval
+    # For voxels in the mask, +1 for positive coefficience with q<0.05 and -1 for negative coefficients with q<0.05
+    3dcalc \
+      -a sm.${sbj}.${GLM}.VisAud_qvals.nii.gz'[0]' -b sm.${sbj}.${GLM}.VisAud_qvals.nii.gz'[1]' \
+      -c ../GroupMask.nii.gz \
+      -expr "ispositive(c)*isnegative(b-0.05)*(ispositive(a)-isnegative(a))" \
+      -overwrite \
+      -prefix sm.${sbj}.${GLM}.VisAud_signif.nii.gz
+    done
+done
+
+
+# Make summary count maps for the smoothed data
+cd $GroupDir
+for GLM in ${GLMlist[@]}; do
+   3dMean -prefix sm.GroupSignifCount_WNW_${GLM}.nii.gz -sum -overwrite \
+        ./sbj_maps/sm.sub-??.${GLM}.WNW_signif.nii.gz
+   3dMean -prefix sm.GroupSignifCount_VisAud_${GLM}.nii.gz -sum -overwrite \
+        ./sbj_maps/sm.sub-??.${GLM}.VisAud_signif.nii.gz
+done
+
+
